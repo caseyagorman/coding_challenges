9acc10b123c4ac6ac1deb7e6666e31b49caeb22a
Casey Gorman <caseyagorman@gmail.com>
2019-01-06 13:03:21 -0800

    add capitalization

---
+++ strings/capitalization.py
@@ -0,0 +1,11 @@
+# Make first letter of the word capitalized
+
+
+def capitalizeWord(word):
+    return word.capitalize()
+
+def capitalizeWord(word):
+    return word.title()
+
+
+# Notes: you can use title or capitalize on a string to capitalize first letter
